b82e09ad1b9c92df58bf03c291cd6608723d752d
linzhonghao <linzhonghao761@163.com>
2023-02-02 09:48:38 +0800

    update 2023/2/2

--- MAKE_input.m
+++ MAKE_input.m
@@ -5,7 +5,8 @@
 % grd.dat dep.dat obc.dat cor.dat in the ./output folder.
 clc;clear all;
 nobc =91;  % nobc = numbers of OBC. nodes
-cartesian = 0 ;  % nobc = numbers of OBC. nodes
+cartesian = 0 ;  % Mobj.nativeCoords = ['spherical'] else ['cartesian'] =1
+Casename = 'Taihai';  % Your FVCOM case name
 %%
 global ftbverbose
 ftbverbose = true;
@@ -13,6 +14,7 @@ addpath('./toolboxs');
 
 if cartesian == 1
    Coords = 'cartesian';
+   Mobj.nativeCoords = 'cartesian';
 else
     Coords = 'spherical';
 end
@@ -21,12 +23,15 @@ Mobj = read_sms_mesh('2dm','.\input\fin8.2dm','coordinate' ,Coords);  % read 2dm
 
 Mobj = add_obc_nodes_list(Mobj,[1:nobc],'OpenOcean',1,1);  %  figure
 
+if cartesian == 0
 Mobj = add_coriolis(Mobj,'uselatitude');  %  The Coriolis force is defined by latitude
+write_FVCOM_cor(Mobj,['./output/',Casename,'_cor.dat']);
+end
 
-write_FVCOM_grid(Mobj,'./output/grd.dat');
-write_FVCOM_bath(Mobj,'./output/dep.dat');
-write_FVCOM_obc(Mobj,'./output/obc.dat');
-write_FVCOM_cor(Mobj,'./output/cor.dat');
+write_FVCOM_grid(Mobj,['./output/',Casename,'_grd.dat']);
+write_FVCOM_bath(Mobj,['./output/',Casename,'_dep.dat']);
+write_FVCOM_obc(Mobj,['./output/',Casename,'_obc.dat']);
+
 
 
 %%
@@ -37,7 +42,7 @@ utmZones = cellfun(@(x) repmat(x, length(Mobj.x), 1), utmZone, 'uni', false);
 [Mobj.lat, Mobj.lon] = utm2deg(Mobj.x, Mobj.y, utmZones{1});
 Mobj.have_lonlat = 1;
 [Mobj] = add_coriolis(Mobj,'uselatitude');
-write_FVCOM_cor(Mobj,'./output/cor.dat');
+write_FVCOM_cor(Mobj,['./output/',Casename,'_cor.dat']);
 end
 
 
